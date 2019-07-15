f3b1bb82ab95e15908967616836445fb6d6995d0
xy <innerfirexy@gmail.com>
2019-07-15 13:37:05 +0800

    add images to assets

--- Assets/Bot.cs
+++ Assets/Bot.cs
@@ -19,36 +19,53 @@ public class Bot : MonoBehaviour,
 	// Use this for initialization
 	void Start () {
         //Log.d(LOG_TAG, "Bot started");
+        isControllerFocus = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //if (isControllerFocus) {
+        //    GetComponent<MeshRenderer>().material.SetColor("_Color", Color.green);
+        //}
+        //else {
+        //    GetComponent<MeshRenderer>().material.SetColor("_Color", Color.red);
+        //}
+    }
 
     public void OnPointerEnter(PointerEventData eventData) {
         Log.d(LOG_TAG, "OnPointerEnter: " + eventData.enterEventCamera.gameObject);
 
-        WaveVR_Controller.EDeviceType type = eventData.enterEventCamera.gameObject.GetComponent<WaveVR_PoseTrackerManager>().Type;
-        GameObject target = eventData.enterEventCamera.gameObject;
+        //WaveVR_Controller.EDeviceType type = eventData.enterEventCamera.gameObject.GetComponent<WaveVR_PoseTrackerManager>().Type;
+        //GameObject target = eventData.enterEventCamera.gameObject;
 
-        if (target.GetComponent<WaveVR_PoseTrackerManager>()) {
-            if (type == WaveVR_Controller.EDeviceType.Dominant) {
-                m_Controller = target;
-                isControllerFocus = true;
-            }
-        }
-
-        if (isControllerFocus) {
-            GetComponent<MeshRenderer>().material.SetColor("_Color", Color.green);
-        }
+        //if (target.GetComponent<WaveVR_PoseTrackerManager>()) {
+        //    if (type == WaveVR_Controller.EDeviceType.Dominant) {
+        //        m_Controller = target;
+        //        isControllerFocus = true;
+        //    }
+        //}
+        GetComponent<MeshRenderer>().material.SetColor("_Color", Color.green);
     }
 
-    public void OnPointerExit(PointerEventData eventDate) {
+    public void OnPointerExit(PointerEventData eventData) {
+        Log.d(LOG_TAG, "OnPointerExit: " + eventData.enterEventCamera.gameObject);
 
+        //WaveVR_Controller.EDeviceType type = eventData.enterEventCamera.gameObject.GetComponent<WaveVR_PoseTrackerManager>().Type;
+        //GameObject target = eventData.enterEventCamera.gameObject;
+
+        //if (target.GetComponent<WaveVR_PoseTrackerManager>()) {
+        //    if (type == WaveVR_Controller.EDeviceType.Dominant){
+        //        m_Controller = target;
+        //        isControllerFocus = false;
+        //    }
+        //}
+        //isControllerFocus = false;
+        GetComponent<MeshRenderer>().material.SetColor("_Color", Color.red);
     }
 
     public void OnPointerHover(PointerEventData eventData) {
-        Log.d(LOG_TAG, "OnPointerHover: " + eventData.enterEventCamera.gameObject);
+        //Log.d(LOG_TAG, "OnPointerHover: " + eventData.enterEventCamera.gameObject);
     }
+
+    
 }
