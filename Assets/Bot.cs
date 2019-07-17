0b20eae3e5b8ed5a73c054fa194cf6e3e66a9786
xy <innerfirexy@gmail.com>
2019-07-17 15:39:01 +0800

    add saving behaviors

--- Assets/Bot.cs
+++ Assets/Bot.cs
@@ -2,44 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using WaveVR_Log;
 using wvr;
 
+
+[RequireComponent(typeof(CanvasGroup))]
 public class Bot : MonoBehaviour,
     IPointerEnterHandler,
     IPointerExitHandler,
     IPointerHoverHandler
 {
     private const string LOG_TAG = "WaveVR_Bot";
-    public bool isControllerFocus;
+    private bool isControllerFocus;
+    private bool isSaved;
+    private bool isReachable;
     WaveVR_Controller.EDeviceType mainControllerType = WaveVR_Controller.EDeviceType.Dominant;
     private CanvasGroup canvasGroup;
 
+    private Image barImage;
+    private Progress progress;
 
-	// Use this for initialization
 	void Start () {
-        //Log.d(LOG_TAG, "Bot started");
         isControllerFocus = false;
+        isSaved = false;
+        isReachable = true;
         canvasGroup = GetComponent<CanvasGroup>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        //if (isControllerFocus) {
-        //    GetComponent<MeshRenderer>().material.SetColor("_Color", Color.green);
-        //}
+        canvasGroup.alpha = 0f;
+        canvasGroup.blocksRaycasts = false;
+
+        barImage = GameObject.Find("bar").GetComponent<Image>();
+        //⚠️ Not clear why transform.Find("bar") causes problem here.
+        barImage.fillAmount = 0.05f;
+        progress = new Progress();
+
+        //Log.d(LOG_TAG, "Bot started");
+        //Log.d(LOG_TAG, "barImage: " + barImage.ToString());
+        //Log.d(LOG_TAG, "barImage fillAmount: " + barImage.fillAmount.ToString());
+        //Debug.Log("Bot started");
+        //Debug.Log("barImage: " + barImage.ToString());
+        //Debug.Log("barImage fillAmount: " + barImage.fillAmount.ToString());
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!isSaved)
+        {
+            if (isControllerFocus && isReachable)
+            {
+                if (WaveVR_Controller.Input(mainControllerType).GetPress(WVR_InputId.WVR_InputId_Alias1_Menu))
+                {
+                    progress.Update();
+                    barImage.fillAmount = progress.GetProgressNorm();
+                    if (barImage.fillAmount >= 0.999f)
+                    {
+                        isSaved = true;
+                    }
+                }
+                if (WaveVR_Controller.Input(mainControllerType).GetPressUp(WVR_InputId.WVR_InputId_Alias1_Menu))
+                {
+                    progress.Reset();
+                    barImage.fillAmount = progress.GetProgressNorm();
+                }
+            }
+            //Use else to handle isReachable == false
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData) {
         //Log.d(LOG_TAG, "OnPointerEnter: " + eventData.enterEventCamera.gameObject);
         isControllerFocus = true;
         ShowCanvas();
+
+        //Make the canvas face towards the VR camera
+        GameObject target = eventData.enterEventCamera.gameObject;
+        Log.d(LOG_TAG, "onPointerEventData target: " + target.transform.localPosition.ToString());
     }
 
     public void OnPointerExit(PointerEventData eventData) {
         //Log.d(LOG_TAG, "OnPointerExit: " + eventData.enterEventCamera.gameObject);
         isControllerFocus = false;
         HideCanvas();
+
+        if (!isSaved) {
+            progress.Reset();
+            barImage.fillAmount = progress.GetProgressNorm();
+        }
     }
 
     public void OnPointerHover(PointerEventData eventData) {
@@ -54,5 +102,30 @@ public class Bot : MonoBehaviour,
     private void ShowCanvas() {
         canvasGroup.alpha = 1f;
         canvasGroup.blocksRaycasts = true;
+    }
+}
+
+
+public class Progress {
+    public const int MAX_PROGRESS = 100;
+
+    private float currentProgress;
+    private float increaseSpeed;
+
+    public Progress() {
+        currentProgress = 5f;
+        increaseSpeed = 30f;
+    }
+
+    public void Update() {
+        currentProgress += increaseSpeed * Time.deltaTime;
+    }
+
+    public void Reset() {
+        currentProgress = 5f;
+    }
+
+    public float GetProgressNorm() {
+        return currentProgress / MAX_PROGRESS;
     }
 }
--- Assets/DebugInfoEvent.cs
+++ Assets/DebugInfoEvent.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Collections;
 using wvr;
 
 
@@ -8,15 +9,28 @@ using wvr;
 public class DebugInfoEvent : MonoBehaviour {
     private Text textField;
     WaveVR_Controller.EDeviceType mainControllerType = WaveVR_Controller.EDeviceType.Dominant;
-
+    private string logMsg;
+    private Queue logMsgQueue = new Queue();
 
     void Awake()
     {
         textField = GetComponent<Text>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    void OnEnable() {
+        //Application.logMessageReceived += HandleLog;
+    }
+
+    void OnDisable() {
+        //Application.logMessageReceived -= HandleLog;
+    }
+
+    private void HandleLog(string logString, string stackTrace, LogType type) {
+        logMsg = logString;
+    }
+
+    // Update is called once per frame
+    void Update () {
         //if (WaveVR_Controller.Input(mainControllerType).GetPress(WVR_InputId.WVR_InputId_Alias1_Trigger))
         //{
         //    textField.text = "Press on Trigger";
@@ -25,5 +39,6 @@ public class DebugInfoEvent : MonoBehaviour {
         //{
         //    textField.text = "Press on Touchpad";
         //}
+        textField.text = logMsg;
     }
 }
